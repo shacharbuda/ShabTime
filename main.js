59e5d8242a8fde1f3338eae68b67686264b9a9f1
Shachar Buda <shacharbuda27@gmail.com>
2019-11-13 20:25:38 +0200

    add location features!

--- main.js
+++ main.js
@@ -5,13 +5,18 @@ const DEFAULT_LOCATION = {
 	lng: 34.850648
 };
 
-var currentLocation = DEFAULT_LOCATION;
+var currentLocation;
 
 const getPosUrl = (lat, lng) => `geo=pos&latitude=${lat}&longitude=${lng}&tzid=${TIMEZONE}`;
 
-$(document).ready(() => {	
-	$('#location-btn').click(() => alert('cliked'));
+$(document).ready(() => {
+	$('#current-location').text('moti')
+	highlightCurrentLocationChange();
+	setCurrentLocation(DEFAULT_LOCATION.lat, DEFAULT_LOCATION.lng);
+	$(".spinner-container").fadeOut();
+	$('#location-btn').click(setCurrentLocationByDeviceLocation);
 	$('#data-btn').click(() => {
+		$(".spinner-container").fadeIn();
 		const posUrl = getPosUrl(currentLocation.lat, currentLocation.lng);
 		$.get(`https://www.hebcal.com/shabbat/?cfg=json&${posUrl}&m=40`, (data) => {
 			handleDataArrived(data);
@@ -21,9 +26,45 @@ $(document).ready(() => {
 	});
 });
 
+const highlightCurrentLocationChange = () => {
+	$('#current-location').on('DOMSubtreeModified', function() {
+		let loopCount = 3;
+		const highlightLoop = () => {
+			if (!loopCount) return;
+			loopCount--;
+			$(this).addClass('highlight-text');
+			setTimeout(() => {
+				$(this).removeClass('highlight-text');
+				setTimeout(() => {
+					highlightLoop();
+				}, 500);
+			}, 300);
+		}
+		highlightLoop();
+	});
+}
+
+const setCurrentLocationByDeviceLocation = () => {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition((position) => {
+			if (position && position.coords && position.coords.latitude && position.coords.longitude) {
+				const {latitude: lat, longitude: lng} = position.coords;
+				setCurrentLocation(lat, lng);
+			}
+		});
+	} else {
+		alert("Geolocation is not supported by this browser.");
+	}
+}
+
+const setCurrentLocation = (lat, lng) => {
+	currentLocation = {lat, lng};
+	$('#current-location').text(`${lat}, ${lng}`);
+}
+
 const handleDataArrived = (data) => {
 	$(".spinner-container").fadeOut();
-	$(".container").fadeIn();
+	$(".data").fadeIn();
 
 	const items = data.items;
 	writeItems(items);
